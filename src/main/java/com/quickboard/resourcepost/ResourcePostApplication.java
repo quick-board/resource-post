9190e2705354193e6bda66cacb11acd6f8cd7be5
seungchan <tmdcks3489@gmail.com>
2025-06-19 18:48:20 +0900

    init

---
+++ src/main/java/com/quickboard/resourcepost/ResourcePostApplication.java
@@ -0,0 +1,13 @@
+package com.quickboard.resourcepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcePostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcePostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickboard/resourcepost/ResourcePostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickboard.resourcepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcePostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
