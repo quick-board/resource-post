d76023873e76dd399fbfade5f7b2147c67d0659a
seungchan <tmdcks3489@gmail.com>
2025-06-26 20:19:50 +0900

    ✨ Add eureka client

--- src/main/java/com/quickboard/resourcepost/ResourcePostApplication.java
+++ src/main/java/com/quickboard/resourcepost/ResourcePostApplication.java
@@ -3,12 +3,14 @@ package com.quickboard.resourcepost;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class ResourcePostApplication {
 
 	public static void main(String[] args) {
