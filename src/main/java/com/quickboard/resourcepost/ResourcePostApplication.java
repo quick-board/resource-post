952c7e1d7e0173c7f7b3eba01357a83a0b14d979
seungchan <tmdcks3489@gmail.com>
2025-08-15 20:27:45 +0900

    🐛 Fix

--- src/main/java/com/quickboard/resourcepost/ResourcePostApplication.java
+++ src/main/java/com/quickboard/resourcepost/ResourcePostApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableDiscoveryClient
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class ResourcePostApplication {
 
 	public static void main(String[] args) {
